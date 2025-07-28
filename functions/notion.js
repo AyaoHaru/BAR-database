e8503f4d7c69b6e2d4e0a716d20639c90a164e0d
AyaoHaru <portiazhang523@gmail.com>
2025-07-28 17:50:43 -0400

    Create notion.js

---
+++ functions/notion.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const NOTION_API_TOKEN = process.env.NOTION_API_TOKEN;
+  const DATABASE_ID = 'your_database_id';
+
+  const body = JSON.parse(event.body);
+
+  const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${NOTION_API_TOKEN}`,
+      'Content-Type': 'application/json',
+      'Notion-Version': '2022-06-28'
+    },
+    body: JSON.stringify({
+      filter: {
+        property: 'ID',
+        rich_text: { equals: body.id }
+      }
+    })
+  });
+
+  const data = await res.json();
+  return {
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify(data)
+  };
+};
